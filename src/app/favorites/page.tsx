481835e4c0efca8f329e6b26d508ed48ed0b8499
SasaB86 <sasabocin86@gmail.com>
2023-09-19 11:10:39 +0200

    adding favorites

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,15 +1,12 @@
-import UploadButton from "./upload-button"
 import cloudinary from "cloudinary"
-import { CloudinaryImage } from "./cloudianryImage";
+import { CloudinaryImage } from "../gallery/cloudianryImage";
+import { SearchResult } from "../gallery/page";
+import { ForceRefresh } from "@/components/force-refresh";
 
-export type SearchResult = {
-    public_id: string
-    tags: string
-}
 
-export default async function GalleryPage() {
+export default async function FavoritesPage() {
    const results = await cloudinary.v2.search
-    .expression('resource_type:image ')
+    .expression('resource_type:image AND tags=favorite')
     .sort_by('created_at','desc')
     .with_field("tags")
     .max_results(30)
@@ -17,15 +14,16 @@ export default async function GalleryPage() {
 
     return (
     <section>
+        <ForceRefresh />
         <div className="flex flex-col gap-8">
             <div className="flex justify-between">
-                <h1 className="text-4xl font-bold">Gallery</h1>
-                <UploadButton/>
+                <h1 className="text-4xl font-bold">Favorite Images</h1>
+    
             </div>
             <div className="grid grid-cols-4 gap-4 mb-4">
                 {results.resources.map((result) => (
                 <CloudinaryImage 
-                    path="/gallery"
+                    path="/favorites"
                     key={result.public_id}
                     src={result.public_id}
                     publicId={result.public_id}
